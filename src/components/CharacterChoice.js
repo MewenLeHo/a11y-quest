b38716fa7b4cd381ba87d18c260c57c1f5e22e55
MewenLeHo <mewen.leho.ext@orange.com>
2025-05-20 13:24:17 +0200

    Fix styles and add characters images

--- src/components/CharacterChoice.js
+++ src/components/CharacterChoice.js
@@ -1,4 +1,5 @@
 import React from "react";
+import styles from './CharacterChoice.module.css';
 
 // Liste des personnages avec leurs stats de départ
 const CHARACTERS = {
@@ -7,28 +8,28 @@ const CHARACTERS = {
     description: "Développeur, maître des outils et du DOM",
     startingGold: 2,
     startingAccessibilityScore: 3,
-    image: "forgeron.jpg", // Ajouter l'image
+    image: "forgeron.png",
   },
   alchimiste: {
     name: "Alchimiste des pixels",
-    description: "Designer, artisan de l’expérience visuelle",
+    description: "Designer, artisan de l'expérience visuelle",
     startingGold: 3,
     startingAccessibilityScore: 2,
-    image: "alchimiste.jpg", // Ajouter l'image
+    image: "alchimiste.png",
   },
   stratège: {
     name: "Stratège du royaume",
     description: "Chef de projet, gestionnaire de budget et de priorités",
     startingGold: 5,
     startingAccessibilityScore: 1,
-    image: "strategiste.jpg", // Ajouter l'image
+    image: "strategiste.png",
   },
   mage: {
     name: "Mage des normes oubliées",
     description: "Expert accessibilité, gardien des grimoires RGAA",
     startingGold: 1,
     startingAccessibilityScore: 5,
-    image: "mage.jpg", // Ajouter l'image
+    image: "mage.png",
   },
 };
 
@@ -36,13 +37,13 @@ function CharacterChoice({ onStart }) {
   return (
     <div className="character-choice">
       <h2>Choisissez votre classe</h2>
-      <ul>
+      <ul className={styles.characterList}>
         {Object.entries(CHARACTERS).map(([key, character]) => (
           <li key={key} className="character-item">
             {/* Affichage de l'image du personnage */}
             <img
-              src={`/images/${character.image}`}
-              alt={character.name}
+              src={`${process.env.PUBLIC_URL}/images/characters/${character.image}`}
+              alt=""
               className="character-image"
             />
             <h3>{character.name}</h3>
